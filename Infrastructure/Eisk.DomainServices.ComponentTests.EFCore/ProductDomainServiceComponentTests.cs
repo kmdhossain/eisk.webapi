f2115b98555c854da0f01f39dcbaf7d609909332
K Hossain <tahs.sumon@gmail.com>
2021-10-19 23:22:39 +0100

    Test updated

--- Infrastructure/Eisk.DomainServices.ComponentTests.EFCore/ProductDomainServiceComponentTests.cs
+++ Infrastructure/Eisk.DomainServices.ComponentTests.EFCore/ProductDomainServiceComponentTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using Eisk.Core.Exceptions;
 using Eisk.DataServices.EFCore;
@@ -17,131 +18,82 @@ namespace Eisk.DomainServices.ComponentTests.EFCore
 
             return productDataService;
         }
-
-        [Fact]
-        public virtual async Task ProductAdd_IsOnlineAnd_WhenProductValueIsMoreThan50_ShouldThrowException()
+    [Fact]// postive test
+    public virtual async Task ProductAdd_ProductIsNotOnline_PriceMoreThan50()
         {
-
-            //Arrange
             var inputProduct = Factory_Entity<Product>();
-            inputProduct.IsOnline = true;
-            const int PRODUCT_PRICE_MORE_THAN = 51;
-            inputProduct.ProductPrice = PRODUCT_PRICE_MORE_THAN;
-
+            inputProduct.IsOnline = false;
+            const int PRODUCT_PRICE_IS_MORE_THAN = 51;
+            inputProduct.ProductPrice = PRODUCT_PRICE_IS_MORE_THAN;
             var productDomainService = new ProductDomainService(Factory_DataService());
 
-            //Act + Assert
-            await Assert.ThrowsAsync<InvalidOperationException>(() =>
-              productDomainService.Add(inputProduct)
-                );
+            //Act
+            var returnProduct = await productDomainService.Add(inputProduct);
+
+            //Assert
+
+            Assert.NotNull(returnProduct);
+            Assert.NotEqual(default(int), returnProduct.ProductId);
+
+
         }
 
-        [Fact]
-        public virtual async Task ProductAdd_IsOnlineAndProductValueIsZero_ShouldThrowException()
+        [Fact] //negative test
+        public virtual async Task ProductAdd_ProductIsOnline_Prize_Is_Less_Than_Zero()
         {
-            //Arrange
             var inputProduct = Factory_Entity<Product>();
             inputProduct.IsOnline = true;
             const int PRODUCT_PRICE_IS_LESS_THAN_ZERO = 0;
             inputProduct.ProductPrice = PRODUCT_PRICE_IS_LESS_THAN_ZERO;
             var productDomainService = new ProductDomainService(Factory_DataService());
-            
-            //Act +Assert
-            await Assert.ThrowsAsync<InvalidDataException<Product>>(() =>
+
+            //Act+Assert
+            await Assert.ThrowsAsync<InvalidDataException<Product>>(()=>
             productDomainService.Add(inputProduct)
-            );
-
+         );
         }
 
+        //Positive boundary test
 
-        // <summary>
-        // Positive test: Test case for product is not online, price is less than 50.
-        // </summary>
         [Fact]
-        public virtual async Task ProductAdd_IsNotOnline_AndPriceIsLessThan50()
-        {
-            //Arrange
 
+        public virtual async Task ProductAdd_ProductIsOnline_PriceEqual50_ShouldNotThrowException()
+        {
             var inputProduct = Factory_Entity<Product>();
             inputProduct.IsOnline = false;
-            const int PRODUCT_PRICE_IS_LESS_THAN = 49;
-            inputProduct.ProductPrice = PRODUCT_PRICE_IS_LESS_THAN;
-            var prodductDomainService = new ProductDomainService(Factory_DataService());
-
-            ///Act
-            var returnProduct = await prodductDomainService.Add(inputProduct);
-            //Assert
-            Assert.NotNull(returnProduct);
-            Assert.NotEqual(default(int), returnProduct.ProductId);
-        }
-
-        //<summary>
-        //Positive test: Test case for product is not online and price is more than 50
-        //</summary>
-        [Fact]
-        public virtual async Task ProductAdd_IsNotOnline_AndPriceIsMoreThan50()
-        {
-            //Arrange
-            var inputProduct = Factory_Entity<Product>();
-            inputProduct.IsOnline = false;
-            const int PRODUCT_PRICE_IS_MORE_THAN = 51;
-            inputProduct.ProductPrice = PRODUCT_PRICE_IS_MORE_THAN;
+            const int PRODUCT_PRICE = 50;
+            inputProduct.ProductPrice = PRODUCT_PRICE;
             var productDomainService = new ProductDomainService(Factory_DataService());
 
-
             //Act
+
             var returnProduct = await productDomainService.Add(inputProduct);
+
             //Assert
-            Assert.NotNull(returnProduct);
-            Assert.NotEqual(default(int), returnProduct.ProductId);
 
-
-        }
-
-        //<summary>
-        //Positive test: product is not online and price is equal to 50, should not throw exception
-        //</summary>
-
-        [Fact]
-        public virtual async Task ProductAdd_ProductIsNotOnline_PriceIsEqual50()
-        {
-
-            //Arrange
-            var inputProduct = Factory_Entity<Product>();
-            inputProduct.IsOnline = false;
-            const int PRODUCT_PRICE_IS = 50;
-            inputProduct.ProductPrice = PRODUCT_PRICE_IS;
-            var productDomainService = new ProductDomainService(Factory_DataService());
-
-
-            //Act
-            var returnProduct = await productDomainService.Add(inputProduct);
-            //Assert
             Assert.NotNull(returnProduct);
             Assert.NotEqual(default(int), returnProduct.ProductId);
 
         }
-        //<summary>
-        //Negative test (boundary):  product is online and price is more than 50 (int.max) (negative tests)
-        //</summary>
+
+        //Negative boundary test
+
         [Fact]
 
-        public virtual async Task ProductAdd_IsOnline_PriceIsMoreThan_50()
+        public virtual async Task ProductAdd_ProductIsOnline_PrizeIsEqualZero_ShouldThrowException()
         {
             var inputProduct = Factory_Entity<Product>();
             inputProduct.IsOnline = true;
-            const int PRODUCT_PRICE_IS_MORE_THAN = 51;
-            inputProduct.ProductPrice = PRODUCT_PRICE_IS_MORE_THAN;
+            const int PRODUCT_PRIZE = 0;
+            inputProduct.ProductPrice = PRODUCT_PRIZE;
             var productDomainService = new ProductDomainService(Factory_DataService());
 
-            //Act +Assert
-            await Assert.ThrowsAsync<InvalidOperationException>(() =>
+            //Act+Assert
+            await Assert.ThrowsAsync<InvalidDataException<Product>>(() =>
             productDomainService.Add(inputProduct)
-            );
 
-
+                );
         }
-
     }
 }
 
